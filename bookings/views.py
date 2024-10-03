adf059df40a20cbd67c248cf8c7bbed6f60115d5
HiraniPJ <pooja.hirani93@gmail.com>
2024-10-03 17:03:39 +0100

    Basic view to display available tables

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Table
 
 # Create your views here.
+def home(request):
+  tables = Table.objects.all()
+  return render(request, 'bookings/home.html', {'tables': tables})
