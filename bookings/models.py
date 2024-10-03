0204748dc5b1a9ae9753bfcf1752a480e8c8a3b5
HiraniPJ <pooja.hirani93@gmail.com>
2024-10-03 16:56:48 +0100

    creating models for table and reservation

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Table(models.Model):
+    number = models.IntegerField()
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return f"Table {self.number} (Seats {self.capacity})"
+    
+class Reservations(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    guests = models.IntegerField()
+
+    def __str__(self):
+        return f"Reservation for {self.user} on {self.date} at {self.time}"
+    
