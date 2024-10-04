eab3da9db8335b79150093f4722dcb47529b0fba
HiraniPJ <pooja.hirani93@gmail.com>
2024-10-04 23:08:10 +0100

    'django.contrib.auth.urls' included to automatically configure all necessary authentications

--- restaurant_booking/urls.py
+++ restaurant_booking/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
 from bookings import views
 
 urlpatterns = [
@@ -25,4 +25,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='bookings/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
