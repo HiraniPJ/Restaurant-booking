5c7060cfab18554b881bfaad99233d174f1a1b47
HiraniPJ <pooja.hirani93@gmail.com>
2024-10-04 22:39:18 +0100

    Signup view to handle user registration

--- bookings/views.py
+++ bookings/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Table
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 
 # Create your views here.
@@ -8,3 +10,15 @@ from .models import Table
 def home(request):
   tables = Table.objects.all()
   return render(request, 'bookings/home.html', {'tables': tables})
+
+def signup(request):
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('home')
+    
+  else:
+    form = UserCreationForm()
+  return render(request, 'bookings/signup.html', {'form': form})
