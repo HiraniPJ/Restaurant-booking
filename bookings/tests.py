5daa92868e414e3aebe25428aa40212bce8eb932
Pooja  Hirani <pooja.hirani93@gmail.com>
2024-10-07 23:02:42 +0000

    Updated Response if invalid

--- bookings/tests.py
+++ bookings/tests.py
@@ -72,7 +72,7 @@ class ViewTests(BaseTestCase):
             'date': '2024-10-12', 'time': '19:00', 'guests': 4      # Missing 'table'
         })
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'table', 'This field is required.')  
+        self.assertContains(response, 'This field is required.')  
             
     def test_my_reservations(self):
         # Test viewing a user's reservations
@@ -132,7 +132,7 @@ class ViewTests(BaseTestCase):
             'guests': ''    # Missing Guests
         })    
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'guests', 'This field is required.')  
+        self.assertContains(response, 'This field is required.')  
 
     def test_delete_reservation(self):
         # Test deleting a reservation
@@ -177,7 +177,7 @@ class SignupViewTests(BaseTestCase):
             'password2': 'Testpassword456'  # Passwords don't match
         })
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Your Passwords didn't match")   
+        self.assertContains(response, "The two password fields didn't match.")   
         
 # Form Tests
 class FormTests(BaseTestCase):
