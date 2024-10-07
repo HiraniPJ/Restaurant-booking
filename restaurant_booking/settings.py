0849f3cb23fdf63b2a697c9bf9e4e7e8ffef1e57
Pooja  Hirani <pooja.hirani93@gmail.com>
2024-10-07 19:51:07 +0000

    connected to the environment variable DATABASE_URL

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -86,6 +86,10 @@ WSGI_APPLICATION = 'restaurant_booking.wsgi.application'
 #    }
 #}
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
