e0acae7cd33ac17cdd829eebdd1e42387bbda657
Pooja  Hirani <pooja.hirani93@gmail.com>
2024-10-09 19:20:44 +0000

    debug, deploy

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 CLOUDINARY_URL = os.environ.get("CLOUDINARY_URL")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 
 ALLOWED_HOSTS = [os.environ.get("HEROKU_HOSTS"), 'localhost']
 
