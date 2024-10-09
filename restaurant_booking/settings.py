5a04715296e81a49c087c8f889439447ad553e58
Pooja  Hirani <pooja.hirani93@gmail.com>
2024-10-09 11:10:42 +0000

    debug = false

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 
 ALLOWED_HOSTS = ['mint-restaurant-app.herokuapp.com', 'localhost', '8000-hiranipj-restaurantbook-6bza5mp4cky.ws-eu116.gitpod.io'
 ]
