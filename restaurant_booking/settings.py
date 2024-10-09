fa19c3c24bd1d76f20726b4006306f8203a9a986
Pooja  Hirani <pooja.hirani93@gmail.com>
2024-10-09 21:54:31 +0000

    DEBUG

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 CLOUDINARY_URL = os.environ.get("CLOUDINARY_URL")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True 
 
 ALLOWED_HOSTS = ['mint-restaurant-app-e407d965e4db.herokuapp.com', 'localhost']
 
