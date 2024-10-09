26b14d1fd308d5db70f8bc21deed42074774da13
Pooja  Hirani <pooja.hirani93@gmail.com>
2024-10-09 11:46:41 +0000

    allowed host origin

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -30,11 +30,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
 
-ALLOWED_HOSTS = ['mint-restaurant-app.herokuapp.com', 'localhost' '8000-hiranipj-restaurantbook-6bza5mp4cky.ws-eu116.gitpod.io'
+ALLOWED_HOSTS = ['mint-restaurant-app.herokuapp.com', 'localhost' 'mint-restaurant-app-e407d965e4db.herokuapp.com'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-   'https://8000-hiranipj-restaurantbook-6bza5mp4cky.ws-eu116.gitpod.io',  # Gitpod URL
+   #'https://8000-hiranipj-restaurantbook-6bza5mp4cky.ws-eu116.gitpod.io',  # Gitpod URL
   'https://mint-restaurant-app.herokuapp.com'  # Heroku URL
 ]
 
